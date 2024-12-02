9a016dadd290cd83a4053fd8752a012f45b6b600
Jesus <jesus@b25>
2024-12-02 10:19:01 +0100

    Clase 2 de diciembre

---
+++ src/org/ies/bank/components/CustomerReader.java
@@ -0,0 +1,23 @@
+package org.ies.bank.components;
+
+import org.ies.bank.model.Customer;
+
+import java.util.Scanner;
+
+public class CustomerReader {
+    private final Scanner scanner;
+
+    public CustomerReader(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public Customer read() {
+        System.out.println("Introduce el NIF");
+        String nif = scanner.nextLine();
+        System.out.println("Introduce tu nombre");
+        String name = scanner.nextLine();
+        System.out.println("Introduce tu apellido");
+        String surname = scanner.nextLine();
+        return new Customer(nif, name, surname);
+    }
+}
